d535ee4edaa7b843d49ce79ad197c1aadd7b7870
Bobrisad <133775575+Bobrisad@users.noreply.github.com>
2024-03-06 17:47:07 +0100

    Add files via upload

---
+++ projducts.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        id: 0,
+        name: "Koszulka Domowa",
+        Decompression: 'Koszulka domowa zawodnika Chomejsky z numerem 11',
+        category: "koszulka domowa",
+        price: 20.00,
+        image: "C:\Users\Michał Chomej\Downloads\koszulka 11",
+    }
+
+{
+    id: 0,
+    name: "Koszulka Domowa",
+    Decompression: 'Koszulka domowa zawodnika Dizel z numerem 69',
+    category: "koszulka domowa",
+    price: 20.00,
+    image: "C:\Users\Michał Chomej\Downloads\koszulka 69",
+}
+]
